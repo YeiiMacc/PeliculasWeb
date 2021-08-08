0e23474a736bddbe3647aa30d4d3de9337daf98e
YeiiMacc <yeiimacc22@outlook.es>
2021-08-07 21:00:52 -0500

    Consumir categoría. Controlador y Vista Index.

---
+++ PeliculasWeb/Controllers/CategoriasController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeliculasWeb.Models;
+using PeliculasWeb.Repositorio.IRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasWeb.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly ICategoriaRepositorio _repoCategoria;
+        public CategoriasController(ICategoriaRepositorio repoCategoria)
+        {
+            _repoCategoria = repoCategoria;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new Categoria() { });
+        }
+    }
+}
